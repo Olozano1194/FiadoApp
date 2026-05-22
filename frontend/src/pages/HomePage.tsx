31250fadd2511c384f6df86eb218a206494aa8fc
Oscar Lozano <olozano1194@gmail.com>
2026-05-22 01:26:55 -0500

    refactor: limpiar HomePage eliminando secciones comentadas y código no utilizado

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,5 @@
-import { useEffect } from "react";
-// import { SEO } from "../components/ui/SEO";
-// import HeroSection from "../components/sections/HeroSection";
-// import ServicesSection from "../components/sections/ServicesSection";
+import HeroSection from "../components/sections/HeroSection";
+import CardsSection from "../components/sections/CardsSection";
 // import ProcessSection from "../components/sections/ProcessSection";
 // import WorkSection from "../components/sections/WorkSection";
 // import FAQSection from "../components/sections/FAQSection";
@@ -9,35 +7,13 @@ import { useEffect } from "react";
 // import PlanSolutionSection from "../components/sections/PlanSolutionSection";
 
 const HomePage = () => {
-    // Scroll a sección si hay hash en la URL
-    useEffect(() => {
-        const hash = window.location.hash;
-        if (hash) {
-            const id = hash.replace("#", "");
-            const element = document.getElementById(id);
-            if (element) {
-                const headerOffset = 64;
-                const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
-                const offsetPosition = elementPosition - headerOffset;
+    
 
-                setTimeout(() => {
-                    window.scrollTo({
-                        top: offsetPosition,
-                        behavior: "smooth",
-                    });
-                }, 100);
-            }
-        }
-    }, []);
     return (
-        <>
-            {/* <SEO
-                image="/logo.png"
-                favicon="/favicon.png"                 
-            />
+        <>             
             <HeroSection />
-            <ServicesSection />
-            <WorkSection />
+            <CardsSection />
+            {/*<WorkSection />
             <ProcessSection />
             <PlanSolutionSection />            
             <FAQSection />
